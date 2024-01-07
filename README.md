6bf640a565f9d0873e8127cda129264fe0d5c5ac
Vilma Hernandez <vilmaglynn@hotmail.com>
2024-01-07 23:00:31 +0000

    added readme file

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# work-day-scheduler
+# Work-day-scheduler
 EDx bootcamp challenge to create a simple calendar application
+
+## Description
+
+The app should:
+Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+* Display the current day at the top of the calender when a user opens the planner.
+ 
+* Present time blocks for standard business hours when the user scrolls down.
+ 
+* Color-code each time block based on past, present, and future when the time block is viewed.
+ 
+* Allow a user to enter an event when they click a time block
+
+* Save the event in local storage when the save button is clicked in that time block.
+
+* Persist events between refreshes of a page
+
+
+## Usage
+
+Link to deployed page
+
+
+
+
+![portfolio](./assets/coding-quiz-image.png)
+
+## License
+MIT License
+
+Copyright (c) 2023 Vilma Hernandez
--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,10 @@
-// save reference to important DOM elements
+// Save reference to important DOM elements
 var currentDay = $("#currentDay");
 var now = new Date().getHours();
 
-console.log(now);
+// Save data
+var storedData = localStorage.getItem("activityInput");
+
 // handle displaying the time
 function displayTime() {
   var rightNow = dayjs().format("DD MMMM YYYY [at] HH:mm:ss");
@@ -13,55 +15,87 @@ function displayTime() {
 $(document).ready(function () {
   // Create column headers
   var tableHeader = $(
-    "<thead><tr><th>Time</th><th>Activity</th><th>Add/Save</th></tr></thead>"
+    "<thead><tr><th>Time</th><th>Activity</th><th>Add/Save</th><th>Delete</th></tr></thead>"
   );
 
   // Create table body
   var tableBody = $("<tbody></tbody>");
 
-  // Generate rows for each hour from 1:00 to 23:00
+  // Generate rows for each hour from 0:00 to 23:00
   for (var i = 0; i <= 23; i++) {
     var time = i + ":00";
 
-    if (i < now) {
-      var row = $(
-        "<tr>" +
-          "<td>" +
-          time +
-          "</td>" +
-          "<td><input type='text' class='activityInput past'></td>" +
-          "<td><button class='styledButton'>Add/Save</button></td>" +
-          "</tr>"
-      );
-      tableBody.append(row);
-    } else if (i === now) {
-      var row = $(
-        "<tr>" +
-          "<td>" +
-          time +
-          "</td>" +
-          "<td><input type='text' class='activityInput present'></td>" +
-          "<td><button class='styledButton'>Add/Save</button></td>" +
-          "</tr>"
-      );
-      tableBody.append(row);
-    } else if (i > now) {
-      var row = $(
-        "<tr>" +
-          "<td>" +
-          time +
-          "</td>" +
-          "<td><input type='text' class='activityInput future'></td>" +
-          "<td><button class='styledButton'>Add/Save</button></td>" +
-          "</tr>"
-      );
-      tableBody.append(row);
+    // Create row with input and buttons
+
+    var row = $(
+      "<tr>" +
+        "<td>" +
+        time +
+        "</td>" +
+        "<td><input type='text' class='activityInput' data-hour='" +
+        i +
+        "'></td>" +
+        "<td><button class='styledButton addSaveButton'>Add/Save</button></td>" +
+        "<td><button class='styledButton deleteButton'>Delete</button></td>" +
+        "</tr>"
+    );
+
+    // Set the input value if there is stored data for this hour
+    if (storedData) {
+      var storedHourData = JSON.parse(storedData);
+      if (storedHourData[i]) {
+        row.find(".activityInput").val(storedHourData[i]);
+      }
     }
+
+    // Add the row to the table body
+    tableBody.append(row);
   }
 
   // Create the table and append it to the container
   var timeTable = $("<table></table>").append(tableHeader).append(tableBody);
   $("#tableContainer").append(timeTable);
+
+  // Save data when the "Add/Save" button is clicked
+  $(".addSaveButton").on("click", function () {
+    var inputField = $(this).closest("tr").find(".activityInput");
+    var hour = inputField.data("hour");
+    var inputValue = inputField.val();
+
+    // Retrieve existing stored data or create an empty object
+    var existingData = JSON.parse(localStorage.getItem("activityInput")) || {};
+
+    // Update the data for the current hour
+    existingData[hour] = inputValue;
+
+    // Save the updated data back to local storage
+    localStorage.setItem("activityInput", JSON.stringify(existingData));
+
+    alert("Data saved successfully!");
+  });
+
+  // Delete data when the "Delete" button is clicked
+  $(".deleteButton").on("click", function () {
+    var inputField = $(this).closest("tr").find(".activityInput");
+    var hour = inputField.data("hour");
+
+    // Retrieve existing stored data
+    var existingData = JSON.parse(localStorage.getItem("activityInput"));
+
+    // Remove the data for the current hour
+    if (existingData && existingData.hasOwnProperty(hour)) {
+      delete existingData[hour];
+      // Save the updated data back to local storage
+      localStorage.setItem("activityInput", JSON.stringify(existingData));
+
+      // Clear the input field value
+      inputField.val("");
+
+      alert("Data deleted successfully!");
+    } else {
+      alert("No data found to delete.");
+    }
+  });
 });
 
 setInterval(displayTime, 1000);
